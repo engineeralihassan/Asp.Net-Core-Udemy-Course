1e096a364448f8297f88968fa909889945df5812
Ali Hassan <engineralihassan@gmail.com>
2023-05-08 18:00:31 +0500

    Responce Headers in Http requests are complete with 1 practical examples

--- 01-Asp.Net Core setup/Program.cs
+++ 01-Asp.Net Core setup/Program.cs
@@ -6,10 +6,41 @@ namespace _01_Asp.Net_Core_setup
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-            string message = "Hi Ali Hope All is well This is the first message from the server side kestral is show in the browser";
-            app.MapGet("/", () => message);
+            string message = "Hi Ali Hope All is well This is the first message" +
+                " from the server side kestral is show in the browser";
+            string message2 = "This is the text of the second Message from the user";
+            //   app.MapGet("/", () => message);// in the place of this line we could also make our custom code and response error s
+            app.Run(async (HttpContext context) =>
+            {
+
+                if (1 == 2)
+                {
+                    context.Response.Headers["Status"] = "Successfull Call ";
+
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync(message);
+                    await context.Response.WriteAsync(message2);
+                }
+                else
+                {
+                    context.Response.Headers["Status"] = "Fail  Requests";
+                    context.Response.Headers["Content-Type"] = "text/html";
+                    context.Response.Headers["Server"] = "Hostinger Desktop123";
+
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("<h3>Sorry You request is not a proper request <h1>401</h1></h3>");
+                    await context.Response.WriteAsync(
+                        "<h4>Try again with proper Text</h4>");
+
+
+                }
+
+
+
+            });
 
             app.Run();
+
         }
     }
 }
