40dbf2c48a9d05b9045e548a0b1f933a26266e3d
Ali Hassan <engineralihassan@gmail.com>
2023-05-10 01:19:33 -0700

    Headers section is complete  complete

--- 01-Asp.Net Core setup/Program.cs
+++ 01-Asp.Net Core setup/Program.cs
@@ -12,14 +12,50 @@ namespace _01_Asp.Net_Core_setup
             //   app.MapGet("/", () => message);// in the place of this line we could also make our custom code and response error s
             app.Run(async (HttpContext context) =>
             {
+                var path = context.Request.Path;
 
-                if (1 == 2)
+                if (1 == 1)
+
                 {
-                    context.Response.Headers["Status"] = "Successfull Call ";
 
+                    // responce  Headers
+                    context.Response.Headers["Status"] = "Successfull Call ";
+                    // content Type 
+                    context.Response.Headers["Content-Type"] = "text/html";
+                    // status codes 
                     context.Response.StatusCode = 200;
+                    // Responce
                     await context.Response.WriteAsync(message);
                     await context.Response.WriteAsync(message2);
+                    // HTTP request Query parameters
+                    if (context.Request.Query.ContainsKey("id"))
+                    {
+                        var id = context.Request.Query["id"];
+                        var name = context.Request.Query["name"];
+
+                        await context.Response.WriteAsync($"<h2>This course id is {id} and name is {name}</h2> ");
+                    }
+
+                    if (path == "/")
+                    {
+
+                        await context.Response.WriteAsync("<p>You are At the Home page</p>");
+
+                    }
+                    else
+                    {
+
+                        await context.Response.WriteAsync("<p>You are not at Home Page </p>");
+                    }
+                    // Request headers
+                    if (context.Request.Headers.ContainsKey("User-Agent"))
+                    {
+                        var userBrowser = context.Request.Headers["User-Agent"];
+                        await context.Response.WriteAsync($"<p> {userBrowser} </p> ");
+
+
+                    }
+
                 }
                 else
                 {
@@ -31,11 +67,53 @@ namespace _01_Asp.Net_Core_setup
                     await context.Response.WriteAsync("<h3>Sorry You request is not a proper request <h1>401</h1></h3>");
                     await context.Response.WriteAsync(
                         "<h4>Try again with proper Text</h4>");
+                    // HTTP request
+
+                    if (path == "/")
+                    {
+
+                        await context.Response.WriteAsync("<p>You are At the Home page </p>");
+
+
+                    }
+                    else
+                    {
+
+                        await context.Response.WriteAsync("<p>You are not at Home Page </p>");
+                    }
 
 
                 }
+                // HTTP request
+
+                if (path == "/")
+                {
+                    context.Response.Headers["Content-Type"] = "text/html";
+                    await context.Response.WriteAsync("<p>You are At the Home page <p>");
 
 
+                }
+                else
+                {
+                    context.Response.Headers["Content-Type"] = "text/html";
+                    await context.Response.WriteAsync("<p>You are not at Home Page <p>");
+                }
+
+                // This is for send the post request from postman
+                context.Response.Headers["Content-type"] = "text/html";
+                //context.Request.Headers["AuthorizationKey"] = "alihassan5063";
+
+
+                // StreamReader reader = new StreamReader(context.Request.Body);
+                //string body = await reader.ReadToEndAsync();
+
+
+
+                // await context.Response.WriteAsync($"<p> Theproperty is  is :  {body}</p> ");
+
+                // await context.Response.WriteAsync($"<p> Theproperty is  is </p> ");
+                //string auth = context.Request.Headers["AuthorizationKey"];
+                //await context.Response.WriteAsync($"<p>The Key is :</p>");
 
             });
 
