4e08dc1533c1c53b7860b390827395accf736d8f
Ali Hassan <engineralihassan@gmail.com>
2023-05-15 14:01:26 -0700

    Controllers and IActionResults continue to learn

--- 05-Controllers/Controllers/IActionResultController.cs
+++ 05-Controllers/Controllers/IActionResultController.cs
@@ -2,25 +2,42 @@
 
 namespace _05_Controllers.Controllers
 {
+    [Controller]
     public class IActionResultController : Controller
     {
+        [Route("book")]
         public IActionResult Index()
         {
-            if (Request.Query.ContainsKey("id"))
+            // Status Code Results 
+
+            if (!Request.Query.ContainsKey("id"))
             {
-                return Content("The Id is note Given by User plese give an propriate id ");
+
+                return BadRequest("<h1>The Id is Not privided </h1>");
+
 
             }
-            if (string.IsNullOrEmpty(Convert.ToString(Request.Query.ContainsKey("id"))))
+            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["id"])))
             {
+                Response.WriteAsync("<h2>Hello Mr you aare wrong request </h2>");
                 return Content("The Id is must be a positive number please give a value to id  ");
 
             }
+            if (Convert.ToBoolean(Request.Query["isLoggedIn"]) != true)
+            {
+                Response.StatusCode = 401;
+                return Content("The User is not Loged in please login first   ");
 
+            }
             // int bookId = Convert.ToInt32(Request.Query["id"]);
             // good way to access the request object 
             int bookId = Convert.ToInt32((ControllerContext.HttpContext.Request.Query["id"]));
-
+            if (bookId <= 0 || bookId > 1000)
+            {
+                Response.WriteAsync("<h2>Hello Mr you aare wrong request </h2>");
+                return Content("<h1>The Id is must be greater then 0 or less then 1000</h1>");
+            }
+            return File("/img.png", "image/png");
         }
     }
 }
