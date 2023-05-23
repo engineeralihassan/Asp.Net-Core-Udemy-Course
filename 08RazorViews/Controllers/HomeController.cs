78d2ffc7890a9eae015052aafea50677953e2d4c
Ali Hassan <engineralihassan@gmail.com>
2023-05-23 15:39:54 -0700

    Razor views are start and continu to learn

---
+++ 08RazorViews/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _08RazorViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View(); // by defuault Views/Home/index.cshtml
+        }
+    }
+}
