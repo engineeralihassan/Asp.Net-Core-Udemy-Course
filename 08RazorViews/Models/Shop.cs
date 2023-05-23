5c6f3cdcf0676fe7560c4ebcd982dea3d42ddd56
Ali Hassan <engineralihassan@gmail.com>
2023-05-23 16:32:22 -0700

    The razor views are continue to learn

---
+++ 08RazorViews/Models/Shop.cs
@@ -0,0 +1,10 @@
+﻿namespace _08RazorViews.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime startDate { get; set; }
+    }
+}
